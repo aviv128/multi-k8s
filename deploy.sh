be6791eda4d2174a4282a22348509537ea56592e
Aviv <3491361+aviv128@users.noreply.github.com>
2020-08-01 20:33:20 +0300

    1

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t aviv128/multi-client:latest -t aviv128/multi-clinet:$SHA -f ./client/Dokerfile ./client
-docker build -t aviv128/multi-server:latest -t aviv128/multi-server:$SHA -f ./server/Dokerfile ./server
-docker build -t aviv128/multi-worker:latest -t aviv128/multi-worker:$SHA -f ./worker/Dokerfile ./worker
+docker build -t aviv128/multi-client:latest -t aviv128/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t aviv128/multi-server:latest -t aviv128/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t aviv128/multi-worker:latest -t aviv128/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push aviv128/multi-client:latest
 docker push aviv128/multi-server:latest
 docker push aviv128/multi-worker:latest
@@ -10,6 +10,6 @@ docker push aviv128/multi-server:$SHA
 docker push aviv128/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployments server=aviv128/multi-server:$SHA
-kubectl set image deployments/client-deployments client=aviv128/multi-client:$SHA
-kubectl set image deployments/worker-deployments worker=aviv128/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=aviv128/multi-server:$SHA
+kubectl set image deployments/client-deployment client=aviv128/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=aviv128/multi-worker:$SHA
