b6a417493d9a6ed12ce506f2c30de9aecf0cda28
Aviv <3491361+aviv128@users.noreply.github.com>
2020-08-01 20:31:35 +0300

    1

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 docker build -t aviv128/multi-client:latest -t aviv128/multi-clinet:$SHA -f ./client/Dokerfile ./client
 docker build -t aviv128/multi-server:latest -t aviv128/multi-server:$SHA -f ./server/Dokerfile ./server
 docker build -t aviv128/multi-worker:latest -t aviv128/multi-worker:$SHA -f ./worker/Dokerfile ./worker
-docker push aviv128/multi-clinet:latest
+docker push aviv128/multi-client:latest
 docker push aviv128/multi-server:latest
 docker push aviv128/multi-worker:latest
 
-docker push aviv128/multi-clinet:$SHA
+docker push aviv128/multi-client:$SHA
 docker push aviv128/multi-server:$SHA
 docker push aviv128/multi-worker:$SHA
 
